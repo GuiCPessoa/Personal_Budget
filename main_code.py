add330a6bbaef1853ba9090a79e2131290379fb5
Guilherme Pessoa <guipessoagith@gmail.com>
2024-09-07 10:25:22 -0300

    Code refactoring

---
+++ main_code.py
@@ -0,0 +1,35 @@
+# Definindo a função para calcular as porcentagens
+def calcular_porcentagens(renda_mensal):
+
+    # Valores das porcentagens
+    valor_necessidade = 50
+    valor_gastos = 30
+    valor_economias = 20
+
+    # Obtendo as porcentagens
+    obter_50_percent = (valor_necessidade / 100) * renda_mensal
+    obter_30_percent = (valor_gastos / 100) * renda_mensal
+    obter_20_percent = (valor_economias / 100) * renda_mensal
+
+    # Retornando resultados
+    return obter_50_percent, obter_30_percent, obter_20_percent
+
+# Função principal para executar o código
+def main():
+
+    # Entrada do usário
+    renda_mensal = float(input("Informe o valor da sua renda mensal: "))
+
+    # Chamar função para calcular porcentagens
+    necessidades, gastos, economias = calcular_porcentagens(renda_mensal)
+
+    # Exibindo resultados
+        
+    print("================\nSeus números são\n================")
+
+    print("Necessidades: R${:,.2f}".format(necessidades))
+    print("Gastos: R${:,.2f}".format(gastos))
+    print("Economias: R${:,.2f}".format(economias))
+
+# Chamando a função principal
+main()
